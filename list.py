74b89feb5d3d576e34836658523fa91153b6ba2a
hemantparihar33 <105932239+hemantparihar33@users.noreply.github.com>
2022-05-20 10:36:51 -0400

    Add files via upload

---
+++ list.py
@@ -0,0 +1,45 @@
+#print("learnig List Option")
+
+#list = ["Phnyle" ,786 ,2.0, "Broom"]
+#print(list)
+#print(list[0])
+#print(list[0:3])
+#print(list[1:])
+#print(list *2)
+
+'''
+
+Plastic_Broom = ["Gala","Milton","3g Clean","Gaba"]
+Dead_Stock = ["Gala","Milton","3g Clean","Gaba"]
+print("Plastic_Broom",Plastic_Broom)
+print("Dead_Stock",Dead_Stock[2:])
+
+Grass_Broom = ["Man Bhavan","Vip","Clean Net","Clear Zip"]
+Grass_Left = [25,15,12,7]
+print("Grass_Broom",Grass_Broom *3)
+print("Grass_Left",Grass_Left[1:3])
+
+Bucket = ["Plastman","Joyo","Agrawal","Saawriya"]
+Stock = ["Plastman 12","Joyo 13","Agrawal 50","Saawriya 39"]
+print("Bucket",Bucket)
+print("Stock",Stock[0:3])
+
+Soap = ["No1","Lux","Dettol","Life Boy"]
+Dead_Stock2 = ["No1 100","Lux 112","Dettol 0","Life Boy 54"]
+print("Soap",Soap)
+print("Dead_Stock2",Dead_Stock2[2])
+
+Talc = ["Ponds","Cinthol","Navratan","Axe"]
+Dead_Stock3 = ["Ponds","Cinthol","Navratan","Axe"]
+print("Talc",Talc)
+print("Dead_Stock3",Dead_Stock3)
+'''
+
+tock = {'Plastman':12,
+         'Joyo': 13,
+         'Agrawal': 50,
+         'Saawriya': 39}
+#tockkeys = tock.keys()
+print(tock.keys())
+
+print (tock.values())
