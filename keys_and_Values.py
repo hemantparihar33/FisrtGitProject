1bea70bf129da194b310db0de24e161521897f53
hemantparihar33 <105932239+hemantparihar33@users.noreply.github.com>
2022-05-21 14:39:11 +0530

    Add files via upload

---
+++ keys_and_Values.py
@@ -0,0 +1,15 @@
+print("keys and Values")
+
+product = {'gala':50,'milton':20,'cello':85,'gaba':36,'kk':43}
+print (product.keys())
+print (product.values())
+
+
+soap = {'lux':50,
+        'no1':20,
+        'dettol':85,
+        'santoor':36,
+        'sevlon':43}
+
+print (soap.keys())
+print (soap.values())
