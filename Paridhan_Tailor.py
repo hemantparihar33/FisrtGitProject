341670d230c7f2c04790c0dbf5f8ffb556d9f585
hemantparihar33 <105932239+hemantparihar33@users.noreply.github.com>
2022-05-25 20:22:00 +0530

    Add files via upload

---
+++ Paridhan_Tailor.py
@@ -0,0 +1,20 @@
+#!usr/bin/python
+from datetime import datetime, time, date
+
+print("Paridhan Tailor")
+
+def Paridhan_Tailor(Serial_No,Bill_No,Name,Mobile_No,Address,Doo,Dod,Curren_Time,Invoice):
+    Serial_No = 2
+    Bill_No = 125
+    Name = Pradumn_Parihar
+    Mobile_No = 123456789
+    Address = Motibanala,Colony
+    DOO = datetime.now()
+    #DOD = 
+    Current_Time = datetime.now()
+    print(Current_Time)
+    Invoice = 750
+    print(Serial_No,Bill_No,Name,Mobile_No,Address,Doo,Dod,Curren_Time,Invoice)
+    return
+
+Paridhan_Tailor(Serial_No,Bill_No,Name,Mobile_No,Address,Doo,Dod,Curren_Time,Invoice)
