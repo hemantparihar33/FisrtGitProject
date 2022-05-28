424528596c9edbd582a2b4d1c973ec5474ce61cf
hemantparihar33 <105932239+hemantparihar33@users.noreply.github.com>
2022-05-28 19:45:25 +0530

    Add files via upload

---
+++ Bachat_Bazar_user_input.py
@@ -0,0 +1,34 @@
+#!usr/bin/python
+from datetime import datetime,date,time,timedelta
+'''
+print("Bachat_Bazar")
+
+def Bachat_Bazar(S_No,Bill_No,Name,Phone,Address,DOO,Load_time,DOD,Invoice):
+    S_No = input("Enter Serial Number:-")
+    Bill_No = 201
+    Name = input("Enter Name:-")
+    Phone = input("Enter Number:-")
+    Address = "Apni Duniya Mai,Mast"
+    DOO = datetime.now()
+    Load_time = datetime.now()
+    DO = timedelta(days=3)
+    DOD = DOO+DO 
+    Invoice = 920
+    print("S_No:-",S_No,"\nBill_No:-",Bill_No,"\nName:-",Name,"\nPhone:-",Phone,"\nAddress:-",Address,"\nDOO:-",DOO,"\nLoad_time:-",Load_time,"\nDOD:-",DOD,"\nInvoice:-",Invoice)
+    return
+
+Bachat_Bazar("S_No","Bill_No","Name","Phone","Address","DOO","Load_time","DOD","Invoice")
+
+'''
+
+a = "hemant"
+if == "parihar":
+    print(a)
+
+
+
+    
+    
+    
+    
+
---
+++ if_esle.py
@@ -0,0 +1,17 @@
+#!usr/bin/python
+
+a = 2+2
+if(a==5):
+    print("True",a,"==4")
+elif(a==4):
+    print("elif wala True",a,"==4")
+else:
+    print("False")
+
+
+
+    
+    
+    
+    
+
