060847a43f465e976c2c878fa3e4567dc82261f3
hemantparihar33 <105932239+hemantparihar33@users.noreply.github.com>
2022-06-13 10:02:07 -0400

    Add files via upload

---
+++ kbc.py
@@ -0,0 +1,13 @@
+#!usr/bin/python
+print("Kon Bnega Krodpati Mai Apka Swagat Hai\nQuestion\nIndia Ki Capital Kya Hai?")
+print ("a = Mumbai\nb = Dewas\nc = Manali\nd = New Delhi")
+a,b,c,d = "a = Mumbai", "b = Dewas", "c = Manali", "d = New Delhi"
+
+response = input("Enter Your Answer\n")
+if(response=='d'or response=='D'):
+    print("Shi Jwaab")
+else:
+    print("Smaya Samapti Ki Seema")
+    
+
+    
